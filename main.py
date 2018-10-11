b1611db4e3d4a84cba1494407ba21a8f99b4a991
Jacopo Tissino <jacopok@gmail.com>
2018-10-11 10:09:25 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  4 15:59:46 2018
+
+@author: jacopo
+"""
+
+import numpy as np
+import scipy
+import sympy as sy
+from sympy import *
+
