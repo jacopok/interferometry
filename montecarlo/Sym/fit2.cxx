da6f15162f4936a75c2f0440fae9d614501aff60
AlessandroLovo <ale571.lovo@gmail.com>
2019-02-26 19:07:39 +0100

    Modificato programma di fit frange

--- montecarlo/Sym/fit2.cxx
+++ montecarlo/Sym/fit2.cxx
@@ -17,14 +17,19 @@ class nu: public ParametricFit::Func {
 	~nu(){}
 	
 	virtual unsigned int n_fix(){return 0;}
-	virtual unsigned int n_unk(){return 3;}// theta_0,n_l,gamma = 
+	virtual unsigned int n_unk(){return 4;}// theta_0,n_l,gamma = lambda/2d
 	virtual double f(double x, vector<double>* v_fix, vector<double>* v_unk){
 	  
-	  double theta_0 = v_unk->at(0);
-	  double n_l = v_unk->at(1);
-	  double gamma = v_unk->at(2);
-	  
-	  double theta = acos((pow(n_l,2) - 1 - pow((gamma*x + n_l - 1),2))/(2*(gamma*x + n_l - 1)));
+	  double n_l = v_unk->at(0);
+	  double gamma = v_unk->at(1);
+	  double theta_0 = v_unk->at(2);
+	  double N_0 = v_unk->at(3);
+	  double theta;
+	  if(x > 0)
+	    theta = acos((pow(n_l,2) - 1 - pow((gamma*(x - N_0) + n_l - 1),2))/(2*(gamma*(x - N_0) + n_l - 1)));
+	  else{
+	    theta = -acos((pow(n_l,2) - 1 - pow((gamma*(-x - N_0) + n_l - 1),2))/(2*(gamma*(-x - N_0) + n_l - 1)));
+	  }
 	  
 	  return theta + theta_0;
 	}
@@ -38,6 +43,7 @@ int main (){
   vector<double>* xV = new vector<double>;
   vector<PDF*>* yVP = new vector<PDF*>;
   vector<double>* pattume = new vector<double>;
+  vector<string> names;
   
   cout << "Where are the data? ";
   cin >> input_name;
@@ -65,8 +71,8 @@ int main (){
   unsigned int n_rep, seed;
   double min_value = 0;
   double param_min, param_max;
-  PDF *n_l, *theta_0, *gamma;
-  MultiPDF *total, *result, *aux1, *aux2;
+  PDF *n_l, *theta_0, *gamma, *N_0;
+  MultiPDF *total, *result, *offsets;
   
   do{
     cout << "Do you want to reset unknown paramters? [y/n] ";
@@ -75,10 +81,6 @@ int main (){
     if(ancora[0] == 'y'){
       pf.delete_unknown_parameters();
       
-      cout << "Insert min and max values for theta_0 and its steps: ";
-      cin >> param_min >> param_max >> datastep;
-      pf.add_unknown_parameter(param_min,param_max,datastep,"theta_0");
-      
       cout << "Insert min and max values for n_l and its steps: ";
       cin >> param_min >> param_max >> datastep;
       pf.add_unknown_parameter(param_min,param_max,datastep,"n_l");
@@ -86,6 +88,14 @@ int main (){
       cout << "Insert min and max values for gamma and its steps: ";
       cin >> param_min >> param_max >> datastep;
       pf.add_unknown_parameter(param_min,param_max,datastep,"gamma");
+      
+      cout << "Insert min and max values for theta_0 and its steps: ";
+      cin >> param_min >> param_max >> datastep;
+      pf.add_unknown_parameter(param_min,param_max,datastep,"theta_0");
+      
+      cout << "Insert min and max values for N_0 and its steps: ";
+      cin >> param_min >> param_max >> datastep;
+      pf.add_unknown_parameter(param_min,param_max,datastep,"N_0");
     }
     
     cout << "Insert mode: value (v), p-value (p) or brute_force (b) (WARNING: brute_force takes time): ";
@@ -109,32 +119,37 @@ int main (){
     }
     
     total = pf.get_unknown_MultiPDF();
-    result = total->integrate_along("gamma","result");
-    aux1 = total->integrate_along("theta_0","aux1");
-    aux2 = total->integrate_along("n_l","aux2");
     
-    n_l = result->integrate_along("tehta_0","n_l")->toPDF();
-    theta_0 = result->integrate_along("n_l","theta_0")->toPDF();
-    gamma = aux1->integrate_along("n_l","gamma")->toPDF();
+    names = vector<string>{"n_l","gamma"};
+    result = total->subMultiPDF(&names,"result");
+    names = vector<string>{"theta_0","N_0"};
+    offsets = total->subMultiPDF(&names,"offsets");
+    
+    n_l = result->integrate_along("gamma","n_l")->toPDF();
+    gamma = result->integrate_along("gamma","n_l")->toPDF();
+    theta_0 = offsets->integrate_along("N_0","theta_0")->toPDF();
+    N_0 = offsets->integrate_along("theta_0","N_0")->toPDF();
     
     cout << "n_l = " << n_l->mean() << " +- " << sqrt(n_l->var()) << endl;
-    cout << "theta_0 = " << theta_0->mean() << " +- " << sqrt(theta_0->var()) << endl;
     cout << "gamma = " << gamma->mean() << " +- " << sqrt(gamma->var()) << endl;
+    cout << "theta_0 = " << theta_0->mean() << " +- " << sqrt(theta_0->var()) << endl;
+    cout << "N_0 = " << N_0->mean() << " +- " << sqrt(N_0->var()) << endl << endl;
     
-    cout << "Correlation coefficient between n_l and theta_0 = " << result->correlation_index() << endl;
-    cout << "Correlation coefficient between n_l and gamma = " << aux1->correlation_index() << endl;
-    cout << "Correlation coefficient between gamma and theta_0 = " << aux2->correlation_index() << endl << endl;
+    cout << "Correlation coefficient between n_l and gamma = " << result->correlation_index() << endl;
+    cout << "Correlation coefficient between N_0 and theta_0 = " << offsets->correlation_index() << endl << endl;
     
     n_l->print("n_l_G.txt");
     gamma->print("gamma_G.txt");
     theta_0->print("theta_0_G.txt");
+    N_0->print("N_0_G.txt");
     result->print("result_G.txt");
-    aux1->print("aux1_G.txt");
-    aux2->print("aux2_G.txt");
+    offsets->print("offsets.txt");
     
     cout << "Do you want to fit again? [y/n]: ";
     cin >> ancora;
   }while(ancora[0] == 'y');
+  
+  total->save("total_MPDF.txt");
   
   cout << endl << endl;
   return 0;
