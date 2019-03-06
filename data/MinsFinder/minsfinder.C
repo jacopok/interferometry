ae4d209644e2deb78c1e68084e56e3f7965db833
lzampieri <zampieri.leonardo98@gmail.com>
2019-03-06 23:48:01 +0100

    Sinus analized

---
+++ data/MinsFinder/minsfinder.C
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <TGraph.h>
+#include <TF1.h>
+
+#define num 3
+
+using namespace std;
+
+void minsfinder(char* inName,char* outName,double lb, double ub) {
+	TGraph* tg = new TGraph(inName);
+	TF1 func ("func","[0] * x * x + [1] * x + [2]");
+	ifstream in(inName);
+	ofstream out(outName);
+
+	double start = 0, end = 0, cont = 0, t1, t2, fr = -1;
+	while( !in.eof() ) {
+		in>>t1>>t2;
+		if ( t2 < lb ) {
+			if ( cont == 0) start = t1;
+			cont ++;
+			end = t1;
+		}
+		if ( t2 > ub ) {
+			if ( cont >= num ) {
+				func.SetParameters(3523,53252,53253);
+				tg->Fit(&func,"Q0","",min(start,end),max(start,end));
+				out<<++fr<<" "<< - func.GetParameter(1) / 2.0 / func.GetParameter(0) <<endl;
+				cout<<start<<" "<<end<<" "<< - func.GetParameter(1) / 2.0 / func.GetParameter(0) << " "<<tg->GetN()<< endl;
+			}
+			cont = 0;
+		}
+	}
+}
