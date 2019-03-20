94b9d3a50af8ecebd51b7ced0b442f2a0b574403
AlessandroLovo <ale571.lovo@gmail.com>
2019-03-20 22:46:18 +0100

    Ottimizzazione di QbicFactory

--- montecarlo/Calibrate/tryQ.cxx
+++ montecarlo/Calibrate/tryQ.cxx
@@ -2,17 +2,22 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 #include "QbicFactory.h"
+#include "GaussFactory.h"
 #include "PDF.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]){
   
-  QbicFactory f1(1,1.001,1);
+  QbicFactory f1(1,1,1);
   PDF* q1 = f1.create_default(200);
   q1->print("q1_G.txt");
+  GaussFactory g(1,sqrt(q1->var()));
+  PDF* g1 = g.create_default(200);
+  g1->print("g1_G.txt");
   
   QbicFactory f2(1,2,1);
   PDF* q2 = f2.create_default(200);
--- montecarlo/Sources/QbicFactory.cc
+++ montecarlo/Sources/QbicFactory.cc
@@ -44,15 +44,16 @@ double QbicFactory::f (double x) const{//unused
 
 PDF* QbicFactory::create(double min, double max, unsigned int steps, const std::string& name) const {
   TriangularFactory f1(center/2,D1);
-  PDF* t1 = f1.create_default(4*steps);
+  PDF* t1 = f1.create_default(2*steps);
   TriangularFactory f2(center/2,D2);
-  PDF* t2 = f2.create_default(3*steps);
+  PDF* t2 = f2.create_default(2*steps - 1);
   vector<PDF*> v{t1,t2};
   
   Convoluter c(&v);
-  unsigned int n = (steps > 250) ? 1e06 : 0;
+  unsigned int n = (steps > 500) ? 1e06 : 0;
   PDF* fin = c.propagation(n,1,min,max,2*steps,name);
   fin->coarse(2);
+  fin->normalize();
   
   return fin;
 }
