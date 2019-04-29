0b38b9cc0d52cda5d38f20f1b0174f93cd58f54e
Jacopo Tissino <jacopok@gmail.com>
2019-04-29 14:37:07 +0200

    Updated simulation functions

--- simulate.py
+++ simulate.py
@@ -6,14 +6,24 @@ Created on Thu Mar 14 14:09:11 2019
 @author: jacopo
 """
 
+from simulation_functions import *
+import Fringes_dataset_manager as fdm
+import initialization as init
+import numpy as np
+
 thickness = 1e-2 #m
 wavelength = 532e-9 #m
 index = 1.33
 
 gamma = wavelength /2 /thickness
 
+index_b = 1.5
+gamma_b = gamma # to fix!!
+index_l = 1.426
+gamma_l = gamma
+
 #cfac = 42.6e-6
-cfac = 51e-6
+cfac = 5.11928e-5
 
 Nsims = 50
 maxfringe = 80
@@ -21,10 +31,6 @@ Ndata = 2*maxfringe
 
 np.random.seed(10414)
 
-index_array = []
-
 for s in range(Nsims):
-    data = measure_sim(fringes, 1.5, 3e-5, 1.33, gamma)
-    p = data.signal.fit()
-    a = (p[3]-1) / (p[2] * p[3])
-    index_array.append(1/(1-a * gamma))
+    data = measure_sim(maxfringe, index_b, gamma_b, index_l, gamma_l, cfac)
+    
--- simulation_functions.py
+++ simulation_functions.py
@@ -10,12 +10,12 @@ import Fringes_dataset_manager as fdm
 import initialization as init
 from numpy.random import uniform
 from numpy.random import normal
+from numpy.random import triangular
 
 def theta_from_fringes_medium(N, index, gamma):
     """
     Inverse function of fringes_difference_medium, still for a generic medium
     """
-    from math import isnan
     Nabs = np.abs(N)
     cos_arg = (index**2 - 1 - (gamma * Nabs + index - 1)**2)/(2*(gamma*Nabs+index-1))
     cos_arg = np.clip(cos_arg, -1, 1)
@@ -31,14 +31,16 @@ def fringes_from_theta_medium(theta, index, gamma):
                                        + np.sqrt(index**2 - np.sin(theta)**2))
     return(n)
 
-def dirty_angles(angles, step_noise=cfac, gain_noise=10/6151):
-    noise1 = uniform(-step_noise, step_noise, Ndata)
-    noise2 = normal(loc=1,scale=gain_noise, size=1)
-    angles += noise1
-    angles *= noise2
-    return(angles)
+def dirty_angles(angles, half_width_fraction=0.25):
+    angle_diffs = np.gradient(angles)
+    def da(a, ad):
+        hw = ad * half_width_fraction
+        triangular_sample = triangular(a-hw, a, a+hw)
+        return(triangular_sample)
+    new_angles = da(angles, angle_diffs)
+    return(new_angles)
     
-def dataset_sim(n_fringes, index, gamma, cfac=cfac):
+def dataset_sim(n_fringes, index, gamma, cfac):
     fringes = np.arange(-n_fringes, n_fringes)
     angles = theta_from_fringes_medium(fringes, index, gamma)
     angles = dirty_angles(angles)
@@ -49,7 +51,16 @@ def dataset_sim(n_fringes, index, gamma, cfac=cfac):
     data.analyze_fine()
     return(data)
 
-def measure_sim(fringes, index_b, gamma_b, index_l, gamma_l, cfac=cfac):
+def measure_sim(fringes, index_b, gamma_b, index_l, gamma_l, cfac):
+    """
+    returns a measure.
+    
+    Inputs:
+    Fringes is a number, the datasets go from -fringes to +fringes
+    indices and gamma for _background and _liquid
+    cfac is the conversion factor, also called alpha
+    """
+    
     bkg = dataset_sim(fringes, index_b, gamma_b, cfac)
     sig = dataset_sim(fringes, index_l, gamma_l, cfac)
     
