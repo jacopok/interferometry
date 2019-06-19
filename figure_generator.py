27d92927e74e823e7069f0122985a8b1e0f1c8ff
jacopo <jacopok@gmail.com>
2019-06-19 21:10:54 +0200

    Updated figure generation, added 2d plot of step vs pdf

--- figure_generator.py
+++ figure_generator.py
@@ -119,13 +119,14 @@ def plot_cloud(fringes, steps, params, pdf, errors=None, radius=None, xlims=None
     for par, pdf_par in zip(params, pdf):
         if(pdf_par>1e-5):
             steps_fit = step_from_fringes(test_fringes, gamma, alpha, *par)
-            plt.plot(test_fringes, steps_fit, alpha=pdf_par, c='b')
+            plt.plot(test_fringes, steps_fit, alpha=pdf_par, c='royalblue')
             counter += 1
 
     if(errors is not None):
-        plt.errorbar(fringes[mask], steps[mask], yerr=errors[mask],**kwargs, label='Data points')
+        plt.errorbar(fringes[mask], steps[mask], yerr=errors[mask],**kwargs,
+            label='Data points',color='yellowgreen')
     else:
-        plt.plot(fringes[mask], steps[mask], 'ro', label = 'Data points')
+        plt.plot(fringes[mask], steps[mask], 'o', color='yellowgreen', label = 'Data points')
 
     if(kwargs['ms'] is not None):
         plt.legend(markerscale= 5 / kwargs['ms'])
@@ -142,8 +143,8 @@ def plot_cloud(fringes, steps, params, pdf, errors=None, radius=None, xlims=None
         plt.close('all')
     return counter
 
-#@timeit
-def find_point_plot(xlims, pdf_location, filename, **kwargs):
+@timeit
+def find_point_3dplot(xlims, pdf_location, filename, **kwargs):
     mask1 = fringes_bp3p3 > xlims[0]
     mask2 = fringes_bp3p3 < xlims[1]
     mask = mask1 * mask2
@@ -152,6 +153,18 @@ def find_point_plot(xlims, pdf_location, filename, **kwargs):
     pdf_steps, pdf_values = PDF_reader.reader_pdf(pdf_location)
 
     plot3d(fringe, step, params_bp3p3, pdf_bp3p3, pdf_steps, pdf_values,
+        figname=filename, xlims=xlims, **kwargs)
+
+@timeit
+def find_point_2dplot(xlims, pdf_location, filename, **kwargs):
+    mask1 = fringes_bp3p3 > xlims[0]
+    mask2 = fringes_bp3p3 < xlims[1]
+    mask = mask1 * mask2
+    fringe = fringes_bp3p3[mask]
+    step = steps_bp3p3[mask]
+    pdf_steps, pdf_values = PDF_reader.reader_pdf(pdf_location)
+
+    plot2d(fringe, step, params_bp3p3, pdf_bp3p3, pdf_steps, pdf_values,
         figname=filename, xlims=xlims, **kwargs)
 
 
@@ -174,11 +187,49 @@ def plot3d(fringe, step, params, pdf, pdf_steps, pdf_values,
     for par, pdf_par in zip(params, pdf):
         if(pdf_par>1e-5):
             steps_fit = step_from_fringes(test_fringes, gamma, alpha, *par)
-            plt.plot(test_fringes, steps_fit, pdf_par/20, alpha=2*pdf_par, c='b')
+            plt.plot(test_fringes, steps_fit, #pdf_par/20,
+            alpha=2*pdf_par, c='royalblue')
 
     for s, p in zip(pdf_steps, pdf_values):
-       ax.plot([fringe, fringe], [s, s], [0, p], color='green')
+       ax.plot([fringe, fringe], [s, s], [0, p], color='yellowgreen')
 
+    # plt.tight_layout()
+    if(figname):
+        fig.savefig(figname, format = 'pdf')
+    if(show==True):
+        plt.show()
+    else:
+        plt.close(fig=fig)
+
+def plot2d(fringe, step, params, pdf, pdf_steps, pdf_values,
+           figname=None, gamma=2.68895e-5, alpha=51.1928e-6, show=True, **kwargs):
+
+    fig = plt.figure()
+
+    plt.xlabel('Steps [1]')
+    plt.ylabel('PDFs')
+
+    plt.plot(step, 0, 'ro', label='Data point')
+
+    xlims = kwargs['xlims']
+    # to generalize
+
+    step_array = []
+    weight_array = []
+    for par, pdf_par in zip(params, pdf):
+        if(pdf_par>1e-5):
+            step_array.append(step_from_fringes(fringe, gamma, alpha, *par))
+            weight_array.append(pdf_par)
+
+    plt.hist(pdf_steps, bins=correct_bins(pdf_steps),
+        weights=pdf_values, label='Datapoint PDF [1/step]',
+        alpha=0.8, color='yellowgreen')
+
+    plt.hist(np.array(step_array).flatten(),
+        weights=np.array(weight_array)/10, label='Curve cloud pdf [10/step]',
+        alpha=0.8, color='royalblue')
+
+    plt.legend()
     # plt.tight_layout()
     if(figname):
         fig.savefig(figname, format = 'pdf')
@@ -207,12 +258,21 @@ def pdf_average_plot(Glist, **kwargs):
     for n in Glist[1:]:
         x, pdf = PDF_reader.reader_pdf(n)
         mask = pdf>pdf_threshold
-        plt.hist(x[mask],weights=pdf[mask], bins=correct_bins(x[mask]), alpha=0.4)
+        plt.hist(x[mask],weights=pdf[mask], bins=correct_bins(x[mask]), alpha=0.3)
+
+    if(len(Glist)>1):
+        avglabel='Average'
+    else:
+        avglabel=None
 
     average_x, average_pdf = PDF_reader.reader_pdf(Glist[0])
     mask = average_pdf>pdf_threshold
     plt.hist(average_x[mask], weights=average_pdf[mask],
-        bins=correct_bins(average_x[mask]), label='Average')
+        bins=correct_bins(average_x[mask]), color='royalblue', label=avglabel, alpha=0.8)
+
+    if(not kwargs.get('nonscientific')):
+        plt.ticklabel_format(axis='x', style='sci', scilimits=(-2,2))
+        plt.ticklabel_format(axis='y', style='sci', scilimits=(-2,2))
 
     plt.legend()
 
@@ -225,34 +285,33 @@ def pdf_average_plot(Glist, **kwargs):
 
 if __name__ == '__main__':
 
-    # plot_cloud(fringes_bp3p3, steps_bp3p3, params_bp3p3, pdf_bp3p3,errors = errors_bp3p3,
-    #     radius = 20,  figname =  'figs/fit_cloud_bp3p3.pdf', show=False,
-    #     fmt='ro', ms=2, capsize=1.5, elinewidth=1, markeredgewidth=0.5)
-    #
-    # ylims = [-800, -750]
-    #
-    #
-    # plot_cloud(fringes_bp3p3, steps_bp3p3, params_bp3p3, pdf_bp3p3, errors = errors_bp3p3,
-    #     xlims=xlims, ylims=ylims, figname = 'figs/fit_zoom_bp3p3.pdf', show=False,
-    #     fmt='ro', ms=10, capsize=5, elinewidth=2, markeredgewidth=2)
-    #
-    # fig = plt.figure()
-    # paraffs['bp3-p3'].plot(radius=0.09)
-    # fig.savefig('figs/bp3p3_measure.pdf', format = 'pdf')
-    # plt.close('')
+    plot_cloud(fringes_bp3p3, steps_bp3p3, params_bp3p3, pdf_bp3p3,errors = errors_bp3p3,
+        radius = 20,  figname =  'figs/fit_cloud_bp3p3.pdf', show=False,
+        fmt='ro', ms=2, capsize=1.5, elinewidth=1, markeredgewidth=0.5)
 
-    # find_point_plot([-10.02, -9.98], 'montecarlo/bp3p3_-10s50.txt', 'figs/3D_qbic_close.pdf', show=False)
-    # find_point_plot([-13.80, -13.75], 'montecarlo/bp3p3_-13s50.txt', 'figs/3D_qbic_far.pdf', show=True)
+    plot_cloud(fringes_bp3p3, steps_bp3p3, params_bp3p3, pdf_bp3p3, errors = errors_bp3p3,
+        xlims=[-10.02, -9.98], ylims=[-800, -750], figname = 'figs/fit_zoom_bp3p3.pdf', show=False,
+        fmt='ro', ms=10, capsize=5, elinewidth=2, markeredgewidth=2)
+
+    fig = plt.figure()
+    paraffs['bp3-p3'].plot(radius=0.09)
+    fig.savefig('figs/bp3p3_measure.pdf', format = 'pdf')
+    plt.close('')
+
+    find_point_3dplot([-10.02, -9.98], 'montecarlo/bp3p3_-10s50.txt', 'figs/3D_qbic_close.pdf', show=False)
+    find_point_3dplot([-13.80, -13.75], 'montecarlo/bp3p3_-13s50.txt', 'figs/3D_qbic_far.pdf', show=False)
+    find_point_2dplot([-10.02, -9.98], 'montecarlo/bp3p3_-10s50.txt', 'figs/2D_qbic_close.pdf', show=False)
+    find_point_2dplot([-13.80, -13.75], 'montecarlo/bp3p3_-13s50.txt', 'figs/2D_qbic_far.pdf', show=False)
 
     gamma_list = ['montecarlo/Gamma/gamma_avg_fix_alpha_G.txt',
-        'montecarlo/Calibrate/fixed_alpha/ba1a1_gamma_G.txt',
-        'montecarlo/Calibrate/fixed_alpha/ba1a2_gamma_G.txt',
-        'montecarlo/Calibrate/fixed_alpha/ba1a3_gamma_G.txt',
-        'montecarlo/Calibrate/fixed_alpha/ba2a1_gamma_G.txt',
-        'montecarlo/Calibrate/fixed_alpha/ba2a2_gamma_G.txt',
-        'montecarlo/Calibrate/fixed_alpha/ba2a3_gamma_G.txt']
+        'montecarlo/Calibrate/fixed_alpha/ba1-a1_gamma_G.txt',
+        'montecarlo/Calibrate/fixed_alpha/ba1-a2_gamma_G.txt',
+        'montecarlo/Calibrate/fixed_alpha/ba1-a3_gamma_G.txt',
+        'montecarlo/Calibrate/fixed_alpha/ba2-a1_gamma_G.txt',
+        'montecarlo/Calibrate/fixed_alpha/ba2-a2_gamma_G.txt',
+        'montecarlo/Calibrate/fixed_alpha/ba2-a3_gamma_G.txt']
 
-    pdf_average_plot(gamma_list, xlabel='$\gamma$ [1]', ylabel='PDF [$1/\gamma$]',
+    pdf_average_plot(gamma_list, xlabel='$\\gamma$ [1]', ylabel='PDF [$1/\\gamma$]',
         figname='figs/gamma.pdf')
 
     n_l_paraff_list = ['montecarlo/n_l_paraffin_average_G.txt',
@@ -266,3 +325,8 @@ if __name__ == '__main__':
 
     pdf_average_plot(n_l_paraff_list, xlabel='$n_l$ [1]', ylabel='PDF [$1/n_l$]',
         figname='figs/n_l.pdf')
+
+    alpha_path = 'montecarlo/Alpha/alpha_G.txt'
+
+    pdf_average_plot([alpha_path], xlabel='$\\alpha$ [rad/step]', ylabel='PDF [$1/\\alpha$]',
+        figname='figs/alpha.pdf')
